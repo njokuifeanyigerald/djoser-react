ca7c2ab5b9e4a32edb122bd2cb275976f3d3ca9e
njokuifeanyigerald <brainboyrichmond@gmail.com>
2020-12-03 15:14:39 +0100

    removed comments in auth.js(actions)

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 
 export const load_user = () => async dispatch => {
-    if (localStorage.getItem('access')) {
+    if(localStorage.getItem('access')){
         const config = {
             headers: {
                 'Content-Type': 'application/json',
@@ -13,104 +13,46 @@ export const load_user = () => async dispatch => {
                 'Accept': 'application/json'
             }
         };
-
         try {
-            const res = await axios.get('/auth/users/me/', config);
-
+            const res = await axios.post('/auth/users/me/',config)
             dispatch({
-                type: USER_LOADED_SUCCESS,
+                type:USER_LOADED_SUCCESS,
                 payload: res.data
-            });
-        } catch (err) {
+            })
+        } catch (error) {
             dispatch({
-                type: USER_LOADED_FAIL
-            });
+                type:USER_LOADED_FAIL
+            })
         }
     } else {
         dispatch({
-            type: USER_LOADED_FAIL
-        });
+            type:USER_LOADED_FAIL
+        })
     }
-};
-
-
-
-// export const load_user = () => async dispatch => {
-//     if(localStorage.getItem('access')){
-//         const config = {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Authorization': `JWT ${localStorage.getItem('access')}`,
-//                 'Accept': 'application/json'
-//             }
-//         };
-//         try {
-//             const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/me/`,config)
-//             dispatch({
-//                 type:USER_LOADED_SUCCESS,
-//                 payload: res.data
-//             })
-//         } catch (error) {
-//             dispatch({
-//                 type:USER_LOADED_FAIL
-//             })
-//         }
-//     } else {
-//         dispatch({
-//             type:USER_LOADED_FAIL
-//         })
-//     }
 
     
-// };
+};
 
-
-export const login = (email, password) => async dispatch => {
+export const login = (email,password) => async dispatch => {
     const config = {
         headers: {
             'Content-Type': 'application/json'
         }
-    };
-
-    const body = JSON.stringify({ email, password });
+    }
+    const body = JSON.stringify({email, password});
 
     try {
-        const res = await axios.post('/auth/jwt/create/', body, config);
-
+        const res = await axios.post('/auth/jwt/create/', body, config)
         dispatch({
-            type: LOGIN_SUCCESS,
+            type:LOGIN_SUCCESS,
             payload: res.data
         });
 
         dispatch(load_user());
-    } catch (err) {
+    }catch (error) {
         dispatch({
-            type: LOGIN_FAIL
-        });
+            type:LOGIN_FAIL
+        })
     }
 };
-
-
-// export const login = (email,password) => async dispatch => {
-//     const config = {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }
-//     const body = JSON.stringify({email, password});
-
-//     try {
-//         const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/create/`, body, config)
-//         dispatch({
-//             type:LOGIN_SUCCESS,
-//             payload: res.data
-//         });
-
-//         dispatch(load_user());
-//     }catch (error) {
-//         dispatch({
-//             type:LOGIN_FAIL
-//         })
-//     }
-// };
 
